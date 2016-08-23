0b5b43b81257cd1108362e14d97e98c3f5ec1d98
Ben Farmer <ben_farmer3@hotmail.com>
2016-08-23 21:46:08 +0100

    Add files via upload

---
+++ GetStudentForCourseTest/GetStudentForCourseTest/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace GetStudentForCourseTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCourses = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.FirstName = new System.Windows.Forms.TextBox();
+            this.SecondName = new System.Windows.Forms.TextBox();
+            this.Age = new System.Windows.Forms.TextBox();
+            this.Gender = new System.Windows.Forms.TextBox();
+            this.CourseId = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.learning_databasesDataSet = new GetStudentForCourseTest.Learning_databasesDataSet();
+            this.tableAdapterManager = new GetStudentForCourseTest.Learning_databasesDataSetTableAdapters.TableAdapterManager();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.learning_databasesDataSet1 = new GetStudentForCourseTest.Learning_databasesDataSet1();
+            this.learningdatabasesDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.learning_databasesDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.learning_databasesDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.learningdatabasesDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Students";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCourses
+            // 
+            this.btnCourses.Location = new System.Drawing.Point(93, 12);
+            this.btnCourses.Name = "btnCourses";
+            this.btnCourses.Size = new System.Drawing.Size(75, 23);
+            this.btnCourses.TabIndex = 1;
+            this.btnCourses.Text = "Courses";
+            this.btnCourses.UseVisualStyleBackColor = true;
+            this.btnCourses.Click += new System.EventHandler(this.btnCourses_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FirstName
+            // 
+            this.FirstName.Location = new System.Drawing.Point(12, 87);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(75, 20);
+            this.FirstName.TabIndex = 4;
+            // 
+            // SecondName
+            // 
+            this.SecondName.Location = new System.Drawing.Point(94, 87);
+            this.SecondName.Name = "SecondName";
+            this.SecondName.Size = new System.Drawing.Size(75, 20);
+            this.SecondName.TabIndex = 5;
+            // 
+            // Age
+            // 
+            this.Age.Location = new System.Drawing.Point(175, 87);
+            this.Age.Name = "Age";
+            this.Age.Size = new System.Drawing.Size(75, 20);
+            this.Age.TabIndex = 6;
+            // 
+            // Gender
+            // 
+            this.Gender.Location = new System.Drawing.Point(256, 87);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(75, 20);
+            this.Gender.TabIndex = 7;
+            // 
+            // CourseId
+            // 
+            this.CourseId.Location = new System.Drawing.Point(337, 87);
+            this.CourseId.Name = "CourseId";
+            this.CourseId.Size = new System.Drawing.Size(75, 20);
+            this.CourseId.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(428, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Insert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // learning_databasesDataSet
+            // 
+            this.learning_databasesDataSet.DataSetName = "Learning_databasesDataSet";
+            this.learning_databasesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.Connection = null;
+            this.tableAdapterManager.CoursesTableAdapter = null;
+            this.tableAdapterManager.RoomsTableAdapter = null;
+            this.tableAdapterManager.StudentsTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = GetStudentForCourseTest.Learning_databasesDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Second Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(175, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Age";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(253, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(334, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Course ID";
+            // 
+            // learning_databasesDataSet1
+            // 
+            this.learning_databasesDataSet1.DataSetName = "Learning_databasesDataSet1";
+            this.learning_databasesDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // learningdatabasesDataSetBindingSource
+            // 
+            this.learningdatabasesDataSetBindingSource.DataSource = this.learning_databasesDataSet;
+            this.learningdatabasesDataSetBindingSource.Position = 0;
+            this.learningdatabasesDataSetBindingSource.CurrentChanged += new System.EventHandler(this.learningdatabasesDataSetBindingSource_CurrentChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 130);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.CourseId);
+            this.Controls.Add(this.Gender);
+            this.Controls.Add(this.Age);
+            this.Controls.Add(this.SecondName);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCourses);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.learning_databasesDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.learning_databasesDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.learningdatabasesDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Learning_databasesDataSet learning_databasesDataSet;
+        private Learning_databasesDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.Button btnCourses;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox FirstName;
+        private System.Windows.Forms.TextBox SecondName;
+        private System.Windows.Forms.TextBox Age;
+        private System.Windows.Forms.TextBox Gender;
+        private System.Windows.Forms.TextBox CourseId;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.BindingSource learningdatabasesDataSetBindingSource;
+        private Learning_databasesDataSet1 learning_databasesDataSet1;
+    }
+}
+
---
+++ GetStudentForCourseTest/GetStudentForCourseTest/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetStudentForCourseTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var studentTableAdapter = new Learning_databasesDataSetTableAdapters.StudentsTableAdapter();
+            var studentsTable = studentTableAdapter.GetDataByCourseId(2);
+
+            StringBuilder allNames = new StringBuilder();
+
+            // Loop through each returned student row and add name/second name to string
+            foreach(var studentRow in studentsTable.Rows.Cast<Learning_databasesDataSet.StudentsRow>())
+            {
+                allNames.AppendLine(studentRow.Name + " " + studentRow.SecondName);
+            }
+
+            MessageBox.Show(allNames.ToString());
+        }
+        private void btnCourses_Click(object sender, EventArgs e)
+        {
+            var courseAdapter = new Learning_databasesDataSetTableAdapters.CoursesTableAdapter();
+            var courseTable = courseAdapter.GetData();
+
+            StringBuilder allClasses = new StringBuilder();
+
+            // Loop through each returned student row and add name/second name to string
+            foreach (var coursesRow in courseTable.Rows.Cast<Learning_databasesDataSet.CoursesRow>())
+            {
+                allClasses.AppendLine(coursesRow.Description );
+            }
+
+            MessageBox.Show(allClasses.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var studentTableAdapter = new Learning_databasesDataSetTableAdapters.StudentsTableAdapter();
+            var studentsTable = studentTableAdapter.GetDataByStudentSecondName(textBox1.Text);
+            StringBuilder allNames = new StringBuilder();
+
+            // Loop through each returned student row and add name/second name to string
+            foreach (var studentRow in studentsTable.Rows.Cast<Learning_databasesDataSet.StudentsRow>())
+            {
+                allNames.AppendLine(studentRow.Name + " " + studentRow.SecondName);
+            }
+
+            MessageBox.Show(allNames.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var studentTableAdapter = new Learning_databasesDataSetTableAdapters.StudentsTableAdapter();
+            var studentsTable = studentTableAdapter.InsertStudent(FirstName.Text, SecondName.Text, int.Parse(Age.Text), Gender.Text, int.Parse(CourseId.Text));
+            
+        }
+
+        private void learningdatabasesDataSetBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
